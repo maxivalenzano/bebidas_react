60a3fbf8c46d822bfcb795b86fe86e43ca3a7c75
maxivalenzano <maxivalenzano@gmail.com>
2020-10-19 18:44:47 -0300

    primeros pasos 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario'
 
 function App() {
   return (
-    <h1>Buscador de bebidas</h1>
+    <Fragment>
+      <Header />
+
+      <div className="container mt-5">
+        <div className="row">
+          <Formulario />
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
