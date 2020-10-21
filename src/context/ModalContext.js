171abba59aae83480fb2b80d9e5ccf1a468b7458
maxivalenzano <maxivalenzano@gmail.com>
2020-10-21 11:29:42 -0300

    consulta api idreceta

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -8,6 +8,20 @@ const ModalProvider = (props) => {
 
     //state del Provider
     const [idreceta, guardarIdReceta] = useState(null);
+    const [receta, guardarReceta] = useState({})
+
+    //llamar a la api con el ID de la receta
+
+    useEffect( () => {
+        const obtenerReceta = async () => {
+            if(!idreceta) return;
+
+            const url=`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`
+            const response = await axios.get(url);
+            guardarReceta(response.data.drinks[0])
+        }
+        obtenerReceta();
+    }, [idreceta])
     
     return ( 
         <ModalContext.Provider
